b5cde6da7206769cd1982fa32429b74edb8401d5
Vasily Gusev <vasily.guseff@gmail.com>
2020-02-16 19:50:41 +0300

    refactor: install react router & bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,23 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import { Route, BrowserRouter as Router, Link } from 'react-router-dom'
+
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const routing = (
+  <Router>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">Contact</Link>
+      </li>
+    </ul>
+    <div>
+      <Route exact path="/" component={App} />
+    </div>
+  </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'))
