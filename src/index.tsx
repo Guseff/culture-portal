ac83bffa72ecc927f6e0803c9c641c224b13d5f4
Olga Bugrova <Lelkabygr@mail.ru>
2020-02-17 15:32:15 +0400

    feat: add main

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,14 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { Route, BrowserRouter as Router, Link } from 'react-router-dom'
 
-import Main from './views/Main'
-import Header from './components/Header/Header'
+
+import Header from './components/header/Header'
+import Main from './components/main/Main'
 
 const routing = (
   <Router>
     <Header />
-    <div>
-      <Route exact path="/" component={Main} />
-    </div>
+    <Main />
   </Router>
 )
 
