68402bb51e3cc5530c0026c4c36349814a384c36
Olga Bugrova <Lelkabygr@mail.ru>
2020-02-17 15:40:09 +0400

    feat: add footer

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,13 @@ import { Route, BrowserRouter as Router, Link } from 'react-router-dom'
 
 import Header from './components/header/Header'
 import Main from './components/main/Main'
+import Footer from './components/footer/Footer'
 
 const routing = (
   <Router>
     <Header />
     <Main />
+    <Footer />
   </Router>
 )
 
