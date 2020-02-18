0a1ba1ef854dad9e1eab80f255bea240ce6ea43d
IgorBarbashov <barbashovis@lad24.ru>
2020-02-18 15:08:43 +0300

    refactor: configure connected-react-router

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,20 @@
 import { createStore } from 'redux';
-import rootReducer from '../reducers';
+import { reducerCreator } from '../reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import { createBrowserHistory } from 'history';
+import { routerMiddleware } from 'connected-react-router';
+
+export const history = createBrowserHistory();
+const reducer = reducerCreator(history);
+const middlewares = [routerMiddleware(history), thunk];
 
 const configureStore = (initialState?: {}) => {
   const store = createStore(
-    rootReducer,
+    reducer,
     initialState,
-    composeWithDevTools(applyMiddleware(thunk))
+    composeWithDevTools(applyMiddleware(...middlewares))
   );
 
   return store;
