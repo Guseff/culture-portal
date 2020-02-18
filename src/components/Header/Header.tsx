7a73c4765a7515bab8a9012edccd2f5da169d83f
Olga Bugrova <Lelkabygr@mail.ru>
2020-02-18 06:59:59 +0400

    feat: add dropdown-menu language

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,20 +5,45 @@ import { Navbar, Nav } from 'react-bootstrap';
 
 const Header = () => {
   return (
-    <Navbar bg="primary" variant="dark">
-      <Navbar.Brand href="/">Logo</Navbar.Brand>
-      <Nav className="mr-auto">
-        <Nav.Link as={Link} to="/">
-          На главную
-        </Nav.Link>
-        <Nav.Link as={Link} to="/list">
-          Писатели
-        </Nav.Link>
-        <Nav.Link as={Link} to="/author">
-          О нас
-        </Nav.Link>
-      </Nav>
-    </Navbar>
+    <header className=" bg-primary">
+      <Navbar>
+        <Navbar.Brand href="/">Logo</Navbar.Brand>
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to="/">
+            На главную
+          </Nav.Link>
+          <Nav.Link as={Link} to="/list">
+            Писатели
+          </Nav.Link>
+          <Nav.Link as={Link} to="/author">
+            О нас
+          </Nav.Link>
+          <div className="dropdown">
+            <button
+              className="btn btn-info dropdown-toggle"
+              type="button"
+              id="dropdownMenu1"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="true">
+              Языки
+              <span className="caret"></span>
+            </button>
+            <ul className="dropdown-menu">
+              <li>
+                <a href="#">ru</a>
+              </li>
+              <li>
+                <a href="#">be</a>
+              </li>
+              <li>
+                <a href="#">eng</a>
+              </li>
+            </ul>
+          </div>
+        </Nav>
+      </Navbar>
+    </header>
   );
 };
 
