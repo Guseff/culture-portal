2151d672cfa93e21fa15573993ff96b7420a895a
Vasily Gusev <vasily.guseff@gmail.com>
2020-02-21 10:06:50 +0300

    feat: get author info from json file added

--- src/views/Author.tsx
+++ src/views/Author.tsx
@@ -1,21 +1,25 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
-
 import { Container, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 import { AppState } from '../types';
 import { getNumberFromRoute } from 'Utils';
 
+import data from '../data/writersData.json';
+
 interface AuthorProps {
   pageFromPath?: string;
 }
 
 const Author: React.FC<AuthorProps> = ({ pageFromPath }) => {
+  const index = getNumberFromRoute(pageFromPath);
+  const writer = data[index];
   return (
     <Container>
       <h3>Author Page</h3>
-      <h4>{`Page number ${getNumberFromRoute(pageFromPath)}`}</h4>
+      <h4>{`Page number ${index}`}</h4>
+      <h3>{writer.name}</h3>
       <Nav.Link as={Link} to={'/list'}>
         Back to Author List
       </Nav.Link>
