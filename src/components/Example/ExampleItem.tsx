7ef0ce704e260f7e70c36e37cbd59d7557e5e004
IgorBarbashov <barbashovis@lad24.ru>
2020-02-21 12:29:45 +0300

    feat: add example page

--- src/components/Example/ExampleItem.tsx
+++ src/components/Example/ExampleItem.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 
 export const ExampleItem = ({ author }: any) => {
-  console.log('in ExampleItem', author);
-
   return (
-    <>
-      {/* <div>{`Name: ${author.name}`}</div>
-      <div>{`Description ${author.description}`}</div> */}
-      <div>noop</div>
-    </>
+    <div>
+      <div>{author.name}</div>
+      <div>{author.description}</div>
+    </div>
   );
 };
--- src/components/Example/ExampleList.tsx
+++ src/components/Example/ExampleList.tsx
@@ -1,48 +1,62 @@
-import React from 'react';
-import { useLocation, useRouteMatch, Link, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, Route } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import { ExampleItem } from './ExampleItem';
 
-const locale = 'ru';
-
 export const ExampleList = () => {
-  const currentPage = useLocation();
-  // const currentMatch = useRouteMatch();
-
-  // console.log('---------------');
-  // console.log('currentPage:', currentPage);
-  // console.log('currentMatch:', currentMatch);
-
   const authorState = useSelector((store: any) => store.author);
   const { byId, author, pending } = authorState;
+
+  const [locale, setLocale] = useState('ru');
 
   if (pending) {
     return <div>Spinner!!!</div>;
   }
 
   return (
-    <div>
-      {byId.length &&
-        byId.map((el: string) => {
-          const myPath = `${currentPage.pathname}/:${el}`;
-          console.log('myPath:', myPath);
-
-          return (
-            <div key={el}>
-              <Link to={`${currentPage.pathname}:${el}`}>
-                {author[el][locale].name}
-              </Link>
-              <Route
-                // exact
-                path={myPath}
-                component={ExampleItem}
-              />
-              {/* <ExampleItem author={author[el][locale]} /> */}
-              {/* </Route> */}
-            </div>
-          );
-        })}
-    </div>
+    <>
+      <div>
+        <span style={{ padding: '10px' }} onClick={() => setLocale('ru')}>
+          Ru
+        </span>
+        <span style={{ padding: '10px' }} onClick={() => setLocale('en')}>
+          En
+        </span>
+        <span style={{ padding: '10px' }} onClick={() => setLocale('be')}>
+          Be
+        </span>
+      </div>
+      {byId.length && (
+        <div
+          style={{
+            display: 'flex',
+            justifyContent: 'flex-start',
+            width: '100%',
+          }}
+        >
+          <div style={{ width: '25%' }}>
+            {byId.map((el: string) => {
+              return (
+                <div key={el}>
+                  <Link to={`/ex/${el}`}>{author[el][locale].name}</Link>
+                </div>
+              );
+            })}
+          </div>
+          <div style={{ width: '75%' }}>
+            {byId.map((el: string) => {
+              return (
+                <div key={el}>
+                  <Route path={`/ex/${el}`}>
+                    <ExampleItem author={author[el][locale]} authorId={el} />
+                  </Route>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
