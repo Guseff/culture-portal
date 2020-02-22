e7f61f53edd24145dcba16fb6f1efd2faf5e75b3
Olga Bugrova <Lelkabygr@mail.ru>
2020-02-22 20:45:22 +0400

    refactor: live coding

--- src/components/About/Card/index.tsx
+++ src/components/About/Card/index.tsx
@@ -2,25 +2,26 @@ import './index.scss';
 import * as React from 'react';
 
 import { Card } from 'react-bootstrap';
-import developers from '../../../data/developersData';
 
 interface DevelopersCardsProps {
-  developer: object[]
+  developer: any;
 }
 
 const Cards: React.FC<DevelopersCardsProps> = ({ developer }) => {
-  return (
-    { developer.map((i) => (
-      <Card>
-        <Card.Img variant="top" src={i.image} />
-        <Card.Body>
-          <Card.Title>{i.name}</Card.Title>
-          <Card.Text>{i.link}</Card.Text>
-          <Card.Text>{i.role}</Card.Text>
-        </Card.Body>
-      </Card>
-    ))}
-  );
+  {
+    developer.map(i => {
+      return (
+        <Card key={i.id}>
+          <Card.Img variant="top" src={i.photo} />
+          <Card.Body>
+            <Card.Title>{i.name}</Card.Title>
+            <Card.Text>{i.link}</Card.Text>
+            <Card.Text>{i.role}</Card.Text>
+          </Card.Body>
+        </Card>
+      );
+    });
+  }
 };
 
 export default Cards;
--- src/views/About.tsx
+++ src/views/About.tsx
@@ -1,6 +1,7 @@
 import '../components/About/index.scss';
 import * as React from 'react';
 import Cards from '../components/About/Card/index';
+import developers from '../data/developersData';
 
 import { Container, Nav } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
@@ -14,7 +15,7 @@ class About extends React.Component {
     return (
       <Container className="content">
         <section className="about">
-          <Cards />
+          <Cards developer={developers} />
         </section>
         <Nav.Link as={Link} to="/worklog">
           Worklog
