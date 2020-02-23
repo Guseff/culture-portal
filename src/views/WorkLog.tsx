40afc13f68895ffba4c1a6b06fd975b1333e18d9
Vasily Gusev <vasily.guseff@gmail.com>
2020-02-23 22:27:47 +0300

    feat: add worklog table

--- src/views/WorkLog.tsx
+++ src/views/WorkLog.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react';
 import { Container } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { AppState } from '../types';
+
+import data from '../data/worklog-data.json';
+import WorkLogTable from '../components/WorkLog/WorkLogTable';
 
 const WorkLog = () => {
+  const settings = useSelector((store: AppState) => store.settings);
+  const { language } = settings;
+
   return (
     <Container className="content">
       <h3>Work Log</h3>
+      <h5>Lang: {language}</h5>
+      <WorkLogTable data={data[language]} />
     </Container>
   );
 };
