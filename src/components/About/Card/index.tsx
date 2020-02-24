bee952b72f32d92eeba84cd77ad9072726fccb75
Olga Bugrova <Lelkabygr@mail.ru>
2020-02-24 23:49:36 +0400

    feat: add github icons and refactor link from about page

--- src/components/About/Card/index.tsx
+++ src/components/About/Card/index.tsx
@@ -1,5 +1,6 @@
 import './index.scss';
 import * as React from 'react';
+import git from '../../../assets/images/icon-git.png';
 
 import { Card } from 'react-bootstrap';
 
@@ -8,12 +9,17 @@ interface DevelopersCardsProps {
 }
 
 const CardItem: React.FC<DevelopersCardsProps> = ({ developer }) => {
+  let itemLink = developer.link.replace('https://github.com/', '');
+
   return (
     <Card>
       <Card.Img variant="top" src={developer.photo} />
       <Card.Body>
         <Card.Title>{developer.name}</Card.Title>
-        <Card.Text>{developer.link}</Card.Text>
+        <div className="card__link">
+          <Card.Img src={git} />
+          <Card.Link href={developer.link}>{itemLink}</Card.Link>
+        </div>
         <Card.Text>{developer.role}</Card.Text>
       </Card.Body>
     </Card>
