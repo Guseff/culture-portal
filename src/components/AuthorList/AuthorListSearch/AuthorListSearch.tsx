013e9668f967f0d592715b6b0dc35814d293722c
Vasily Gusev <vasily.guseff@gmail.com>
2020-02-24 21:50:14 +0300

    feat: add search component translate

--- src/components/AuthorList/AuthorListSearch/AuthorListSearch.tsx
+++ src/components/AuthorList/AuthorListSearch/AuthorListSearch.tsx
@@ -1,31 +1,36 @@
 import React from 'react';
-import { FormControl, Form } from "react-bootstrap";
-import { FILTER_AUTHOR_LIST } from '../../../constants';
+import { FormControl, Form } from 'react-bootstrap';
+import { SEARCH_SEL_TRANSLATES, SEARCH_TRANSLATES } from '../../../constants';
 import './AuthorListSearch.scss';
 
-const selectList = Object.values(FILTER_AUTHOR_LIST)[0];
-
 interface AuthorListSearchProps {
-    handleFilterSelect: (e: React.ChangeEvent<HTMLInputElement>) => any,
-    handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => any
+  handleFilterSelect: (e: React.ChangeEvent<HTMLInputElement>) => any;
+  handleInputChange: (e: React.ChangeEvent<HTMLInputElement>) => any;
+  lang: string;
 }
 
-const AuthorListSearch: React.FC<AuthorListSearchProps> = ({ handleFilterSelect, handleInputChange }) => {
-    return (
-        <Form.Group className="input-group mb-3">
-            <div className="input-group-prepend">
-                <Form.Control as="select" onChange={handleFilterSelect}>
-                    {selectList.map((value, index) => {
-                        return <option key={index}>{value}</option>
-                    })}   
-                </Form.Control>  
-            </div> 
-            <FormControl 
-                type="text" 
-                placeholder="Поиск" 
-                onChange={handleInputChange}/>           
-        </Form.Group>
-    )
-}
+const AuthorListSearch: React.FC<AuthorListSearchProps> = ({
+  handleFilterSelect,
+  handleInputChange,
+  lang,
+}) => {
+  return (
+    <Form.Group className="input-group mb-3 row mx-0">
+      <div className="input-group-prepend col-md-4 px-0">
+        <Form.Control as="select" onChange={handleFilterSelect}>
+          {SEARCH_SEL_TRANSLATES[lang].map((value, index) => {
+            return <option key={index}>{value}</option>;
+          })}
+        </Form.Control>
+      </div>
+      <FormControl
+        className="col-md-8"
+        type="text"
+        placeholder={SEARCH_TRANSLATES[lang]}
+        onChange={handleInputChange}
+      />
+    </Form.Group>
+  );
+};
 
 export { AuthorListSearch };
--- src/constants/common.ts
+++ src/constants/common.ts
@@ -7,5 +7,17 @@ export const LANGUAGES = {
 export const FILTER_AUTHOR_LIST = {
   RU: ['Имя', 'Город рождения'],
   EN: ['Name', 'City of birth'],
-  BE: ['Iмя', 'Горад нараджэння']
+  BE: ['Iмя', 'Горад нараджэння'],
+};
+
+export const SEARCH_SEL_TRANSLATES = {
+  ru: ['По имени', 'По городу рождения'],
+  en: ['By Name', 'By City of birth'],
+  be: ['Па iмяни', 'Па гораду нараджэння'],
+};
+
+export const SEARCH_TRANSLATES = {
+  ru: 'Поиск...',
+  en: 'Search...',
+  be: 'Пошук...',
 };
