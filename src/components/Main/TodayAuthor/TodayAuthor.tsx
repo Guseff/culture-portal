7de7fe769f757c6625a1b1a8372539cae18144ac
Diana Ivanova <din.ivanova13@gmail.com>
2020-02-24 18:25:31 +0300

    refactor: change data place and add currect langs

--- src/components/Main/TodayAuthor/TodayAuthor.tsx
+++ src/components/Main/TodayAuthor/TodayAuthor.tsx
@@ -1,31 +1,25 @@
 import './index.scss';
 
 import React from 'react';
-import { useSelector } from 'react-redux';
-import { Container, Image, Row, Col, Spinner } from 'react-bootstrap';
+import { Container, Image, Row, Col } from 'react-bootstrap';
 import LearnMoreButton from '../LearnMoreButton/';
 
 const currentDay: number = new Date().getDay();
 
-interface IAuthorState {
-  byId: number[];
+interface ITodayAuthorProps {
+  data: string[];
   author: object;
-  pending: boolean;
+  lang: string;
 }
 
 type PrettyDate = (date: string) => string;
 
-const TodayAuthor: React.FC = () => {
-  const authorState: IAuthorState = useSelector((store: any) => store.author);
-  const { byId, author, pending }: IAuthorState = authorState;
-
-  const locale: string = 'ru'; // language will be taken later
-
-  if (pending) {
-    return <Spinner className="spinner" animation="border" />;
-  }
-
-  const id: number = byId[currentDay];
+const TodayAuthor: React.FC<ITodayAuthorProps> = ({
+  data,
+  author,
+  lang,
+}: ITodayAuthorProps) => {
+  const id: string = data[currentDay];
 
   const getPrettyDate: PrettyDate = (date: string) =>
     date
@@ -33,38 +27,32 @@ const TodayAuthor: React.FC = () => {
       .reverse()
       .join('.');
 
-  return (
-    <>
-      {byId.length && (
-        <Container className="today-author">
-          <Row>
-            <Col>
-              <h2 className="author-title">Писатель дня</h2>
-            </Col>
-          </Row>
-          <Row>
-            <Col className="content-col-img" xs={5}>
-              <Image
-                className="author-photo"
-                src={author[id].photo}
-                alt={author[id][locale].name}
-              />
-            </Col>
-            <Col className="content-col-data">
-              <p className="author-name">{author[id][locale].name}</p>
-              <p className="author-date">{`${getPrettyDate(
-                author[id][locale].birthDate
-              )} - ${getPrettyDate(author[id][locale].deathDate)}`}</p>
-              <p className="author-description">
-                {author[id][locale].description}
-              </p>
-              <LearnMoreButton />
-            </Col>
-          </Row>
-        </Container>
-      )}
-    </>
-  );
+  return data.length ? (
+    <Container className="today-author">
+      <Row>
+        <Col>
+          <h2 className="author-title">Писатель дня</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col className="content-col-img" xs={5}>
+          <Image
+            className="author-photo"
+            src={author[id].photo}
+            alt={author[id][lang].name}
+          />
+        </Col>
+        <Col className="content-col-data">
+          <p className="author-name">{author[id][lang].name}</p>
+          <p className="author-date">{`${getPrettyDate(
+            author[id][lang].birthDate
+          )} - ${getPrettyDate(author[id][lang].deathDate)}`}</p>
+          <p className="author-description">{author[id][lang].description}</p>
+          <LearnMoreButton />
+        </Col>
+      </Row>
+    </Container>
+  ) : null;
 };
 
 export default TodayAuthor;
--- src/views/Main.tsx
+++ src/views/Main.tsx
@@ -1,48 +1,59 @@
 import '../components/Main/index.scss';
 
 import * as React from 'react';
-import { Row, Col, Container, Image } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { Row, Col, Container, Spinner } from 'react-bootstrap';
 import TodayAuthor from '../components/Main/';
 
-class Main extends React.Component {
-  title: string;
-  name: string;
+interface IAuthorState {
+  byId: string[];
+  author: object;
+  pending: boolean;
+}
 
-  constructor(props: Readonly<{}>) {
-    super(props);
-    this.title = '';
-    this.name = '';
-  }
+interface ISettingsState {
+  language: string;
+}
 
-  render() {
+const Main: React.FC = () => {
+  const authorState: IAuthorState = useSelector((store: any) => store.author);
+  const settingsState = useSelector((store: any) => store.settings);
+  const { byId, author, pending }: IAuthorState = authorState;
+  const { language }: ISettingsState = settingsState;
+
+  if (pending) {
     return (
       <Container className="content">
-        <Row>
-          <Col className="col_item1">
-            <h2 className="main_title">
-              Культурный портал о писателях Беларуси
-            </h2>
-            <p className="main_description">
-              Культурный портал о писателях Беларуси — сборник статей о
-              выдающихся творческих личностях с их подробным описанием, включая
-              годы жизни, фотографии, биографии в виде временной шкалы, списки
-              авторских работ, видеозаписи, а также карты с обозначением их
-              основной деятельности. Вы можете выбрать автора из списка и узнать
-              о нём подробнее. Кроме того, каждый день меняется писатель в
-              секции «Писатель дня». В графе «О нас» Вы сможете узнать о
-              разработчиках сайта, увидеть ход рабочего процесса и посмотреть на
-              шаблон стилей сайта.
-            </p>
-          </Col>
-        </Row>
-        <Row>
-          <Col>
-            <TodayAuthor />
-          </Col>
-        </Row>
+        <Spinner className="spinner" animation="border" />
       </Container>
     );
   }
-}
+
+  return (
+    <Container className="content">
+      <Row>
+        <Col className="col_item1">
+          <h2 className="main_title">Культурный портал о писателях Беларуси</h2>
+          <p className="main_description">
+            Культурный портал о писателях Беларуси — сборник статей о выдающихся
+            творческих личностях с их подробным описанием, включая годы жизни,
+            фотографии, биографии в виде временной шкалы, списки авторских
+            работ, видеозаписи, а также карты с обозначением их основной
+            деятельности. Вы можете выбрать автора из списка и узнать о нём
+            подробнее. Кроме того, каждый день меняется писатель в секции
+            «Писатель дня». В графе «О нас» Вы сможете узнать о разработчиках
+            сайта, увидеть ход рабочего процесса и посмотреть на шаблон стилей
+            сайта.
+          </p>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <TodayAuthor data={byId} author={author} lang={language} />
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default Main;
