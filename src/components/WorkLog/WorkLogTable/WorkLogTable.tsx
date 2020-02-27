28b1fb7c4f7449ad3a2d0e23e90eecebed9210b3
Olga Bugrova <Lelkabygr@mail.ru>
2020-02-27 13:37:15 +0400

    feat: add styles worklog page

--- src/components/WorkLog/WorkLogTable/WorkLogTable.tsx
+++ src/components/WorkLog/WorkLogTable/WorkLogTable.tsx
@@ -4,26 +4,26 @@ import { Table } from 'react-bootstrap';
 const WorkLogTable = ({ data }) => {
   return (
     <div>
-      <h3>{data.title}</h3>
-      <Table striped bordered hover size="sm">
+      <h3 className="worklog__title">{data.title}</h3>
+      <Table hover>
         <thead>
           <tr>
-            <th>{data.headers.feature}</th>
-            <th>{data.headers.from}</th>
-            <th>{data.headers.to}</th>
-            <th>{data.headers.spent}</th>
-            <th>{data.headers.who}</th>
+            <th scope="col">{data.headers.feature}</th>
+            <th scope="col">{data.headers.from}</th>
+            <th scope="col">{data.headers.to}</th>
+            <th scope="col">{data.headers.spent}</th>
+            <th scope="col">{data.headers.who}</th>
           </tr>
         </thead>
         <tbody>
           {data.body.map((tableRow, i) => {
             return tableRow.feature ? (
               <tr key={i}>
-                <td>{tableRow.feature}</td>
-                <td>{tableRow.from}</td>
-                <td>{tableRow.to}</td>
-                <td>{tableRow.spent}</td>
-                <td>{tableRow.who}</td>
+                <td data-label={data.headers.feature}>{tableRow.feature}</td>
+                <td data-label={data.headers.from}>{tableRow.from}</td>
+                <td data-label={data.headers.to}>{tableRow.to}</td>
+                <td data-label={data.headers.spent}>{tableRow.spent}</td>
+                <td data-label={data.headers.who}>{tableRow.who}</td>
               </tr>
             ) : null;
           })}
