f2735ebdc3385b173155d02906f2007549b7d08b
Diana Ivanova <din.ivanova13@gmail.com>
2020-02-28 03:51:46 +0300

    feat and refactor: add styleguide overview at page and update btn functional

---
+++ src/components/StyleGuide/StyleGuideOverview/StyleGuideOverview.tsx
@@ -0,0 +1,33 @@
+import './index.scss';
+
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
+import RedirectButton from '../../RedirectButton';
+
+const StyleGuideOverview = () => {
+  const { t } = useTranslation();
+
+  return (
+    <>
+      <h3 className="styleguide__title">{t('styleguide')}</h3>
+      <p className="styleguide__overview">{t('styleguideOverview')}</p>
+      <Row>
+        <Col>
+          <p className="styleguide__text">{t('styleguideLink')}</p>
+          <RedirectButton as="a" link="/" text={t('redirect')} />
+        </Col>
+        <Col>
+          <p className="styleguide__text">{t('styleguidist')}</p>
+          <RedirectButton
+            as="a"
+            link="https://react-styleguidist.js.org/"
+            text={t('redirect')}
+          />
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default StyleGuideOverview;
