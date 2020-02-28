b58c5e09761a97fb6c5624081a3b239bed5e8c83
Vasily Gusev <vasily.guseff@gmail.com>
2020-02-28 15:09:54 +0300

    feat: add worklog from cms

--- src/views/WorkLog.tsx
+++ src/views/WorkLog.tsx
@@ -1,23 +1,35 @@
 import '../components/WorkLog/index.scss';
 import * as React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Spinner } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
-import { IStoreState } from '../types';
+import { IStoreState, IWorkLog } from '../types';
 
-import data from '../data/worklog-data.json';
+// import data from '../data/worklog-data.json';
 import WorkLogDifficulties from '../components/WorkLog/WorkLogDifficulties';
 import WorkLogSelfCheck from '../components/WorkLog/WorkLogSelfCheck';
 import WorkLogTable from '../components/WorkLog/WorkLogTable';
 
 const WorkLog = () => {
+  const worklogObj: IWorkLog = useSelector(
+    (store: IStoreState) => store.worklog
+  );
   const settings = useSelector((store: IStoreState) => store.settings);
   const { language } = settings;
+  const { pending, worklog } = worklogObj;
+
+  console.log(worklog);
 
   return (
     <Container className="content content-worklog">
-      <WorkLogTable data={data[language]} />
-      <WorkLogDifficulties language={language} />
-      <WorkLogSelfCheck language={language} />
+      {pending ? (
+        <Spinner className="spinner" animation="border" />
+      ) : (
+        <>
+          <WorkLogTable data={worklog[language]} />
+          <WorkLogDifficulties language={language} />
+          <WorkLogSelfCheck language={language} />
+        </>
+      )}
     </Container>
   );
 };
